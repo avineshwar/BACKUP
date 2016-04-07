c6f62ecba443a14dcac38b7c172656ef6bf7050d
Avineshwar Pratap Singh <avineshwar@live.in>
2016-04-07 12:38:25 -0400

    Design outlined.

---
+++ final-in-progress.sh
@@ -0,0 +1,25 @@
+# this is the "almost" base script. I will update it for at least one method in some time.
+
+while getopts 'hm:v:' flag; do
+        case ${flag} in
+                m)
+                      #  egrep -o '([0-9a-fA-F][0-9a-fA-F]:){4,5}([0-9a-fA-F][0-9a-fA-F])'
+                      #  exit 0
+                      #  #doesn't works correctly on OmniOS. After fixing, check for other OSes too.
+                ;;
+                h)      echo hello. This is help.
+                ;;
+                v)      volume_id="${OPTARG}"
+                        if [ ! -z $avineshwar ]
+                        then
+                                echo not empty.
+                        fi
+                        echo $avineshwar was the argument
+
+                ;;
+                *)
+                        echo Wrong flag chosen.
+                        exit 1
+                ;;
+        esac
+done
