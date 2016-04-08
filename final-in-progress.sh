d79c166dac6dcdd7451f645eba92d538efa9b273
Avineshwar Pratap Singh <avineshwar@live.in>
2016-04-08 01:11:00 -0400

    Updated.

--- final-in-progress.sh
+++ final-in-progress.sh
@@ -1,13 +1,57 @@
 # this is the "almost" base script. I will update it for at least one method in some time.
 
+# SET VARIABLES
+verbose=$EC2_BACKUP_VERBOSE
+key=$EC2_BACKUP_FLAGS_SSH
+instance_type=$EC2_BACKUP_FLAGS_AWS
+
+dir_to_backup="$1"
+
+backup_volume_size=`du -s dir_to_backup 2>/dev/null | cut -f1`
+backup_volume_size=`echo $(($dir_to_backup * 2))`
+backup_volume_size=`echo $(($dir_to_backup / 1024))`
+backup_volume_size=`echo $(($dir_to_backup / 1024))`
+backup_volume_size=`echo $(($dir_to_backup + 1))`
+
+if [ -z "key" ]
+then
+		aws ec2 create-key-pair --key-name $USER-EC2-BACKUP-key --query 'KeyMaterial' --output text > $USER-EC2-BACKUP-key.pem
+		chmod 400 $USER-EC2-BACKUP-key.pem
+		key=$USER-EC2-BACKUP-key
+		
+		# create security group as well.
+		# use "&&" or ";" if making sure the first command exited successfully, is necessary.
+		# "&&" won't let the second command execute if, the first one fails.
+		aws ec2 create-security-group --group-name $USER-EC2-BACKUP-group --description "EC2-BACKUP-tool" > /dev/null && aws ec2 authorize-security-group-ingress --group-name $USER-EC2-BACKUP-group --port 22 --protocol tcp --cidr 0.0.0.0/0 > /dev/null
+else
+		aws ec2 create-security-group --group-name $USER-EC2-BACKUP-group --description "EC2-BACKUP-tool" > /dev/null && aws ec2 authorize-security-group-ingress --group-name $USER-EC2-BACKUP-group --port 22 --protocol tcp --cidr 0.0.0.0/0 > /dev/null
+fi
+
 while getopts 'hm:v:' flag; do
         case ${flag} in
-                m)
-                        # methods should go in here. follow every acceptable check.
-                ;;
                 h)      # This should go first.
                         echo hello. This is help.
                         exit 0
+                ;;
+                
+				m)		method="${OPTARG}"
+						if [ "$method" == "dd" -o -z "$method" ]
+						then
+								# done above. dir_to_backup="$1"
+								# spinning a NetBSD instance.
+								aws ec2 run-instances --image-id ami-569ed93c --key-name $key --security-groups $USER-EC2-BACKUP-group --count 1 $EC2_BACKUP_FLAGS_AWS > tee
+								availability_zone=`cat tee | egrep -o 'us-.{6,7}|eu-.{6,10}|ap-.{11,12}|sa-.{6,7}'`
+								instance_id=`cat tee | egrep -o '\Wi-.{8}' | egrep -o 'i-.{8}'`
+								volume_id=`aws ec2 create-volume --size $backup_volume_size --volume-type gp2 --availability-zone $availability_zone | egrep -o 'vol-.{8}'`
+								aws ec2 attach-volume --volume-id $volume_id --instance-id $instance_id --device /dev/sdf
+								echo "Going to sleep"
+								sleep 225
+								echo "Woke up"
+								# fetch public-ip/public-dns to log in"
+								tar zvcf - $dir_to_backup | ssh -o StrictHostKeyChecking=no -i $key root@public-ip/public-dns "back_vol=`dmesg|tail - 1 | cut -d : -f1`` ; dd of=/dev/$back_vol" | 2>/dev/null
+								echo $?
+								exit 0
+                        # methods should go in here. follow every acceptable check.
                 ;;
                 
                 v)      volume_id="${OPTARG}"
@@ -16,7 +60,6 @@ while getopts 'hm:v:' flag; do
                                 echo "reachable code."
                         fi
                         echo "$volume_id was the argument"
-                        exit 0
                 ;;
 
                 *)
